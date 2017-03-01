c5e1ed92cb8b45213ad585e5202bc54aa1520b8a
zzx <315084425@qq.com>
2017-03-01 10:55:48 +0800

    header完成，第一次提交

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="header">
+		我是goods
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/css">
+	
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="header">
+		我是ratings
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/css">
+	
+</style>
